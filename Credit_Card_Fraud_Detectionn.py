19c3a7cf653824147c6df86bb061a5b72990b0c9
eyeameer <mohdameer788@gmail.com>
2022-10-18 18:26:04 +0530

    first commit

---
+++ Credit_Card_Fraud_Detectionn.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:66bc9af8024ac0fd95a25feed4c451f618937f1b9a50d88bdce9b50cb94d00e9
+size 31054
---
+++ Diamond Price Prediction.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:36ac454214f0c60a690a1de4ed297c876772d20c581764c532236bd99a069aa5
+size 6229
